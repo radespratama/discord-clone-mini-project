03a895e196831cb292eee46b034dd59189ab2838
radespratama <puturades89@gmail.com>
2021-03-14 09:17:43 +0800

    [Rades] First Commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCQfktHjxFo0jZ1OjDvleniflPnkKkSFzM",
+    authDomain: "discord-clone-personal.firebaseapp.com",
+    projectId: "discord-clone-personal",
+    storageBucket: "discord-clone-personal.appspot.com",
+    messagingSenderId: "616540798506",
+    appId: "1:616540798506:web:8dd1336da756194bce8d53",
+    measurementId: "G-5CJQPCCGFT"
+};

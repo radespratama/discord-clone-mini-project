b4e6eecbd9160ba72fba9c214a923e85a1906a9b
radespratama <puturades89@gmail.com>
2021-03-15 10:32:24 +0800

    [Rades] Clear Chat Section Part 1

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,11 +1,61 @@
 import React from 'react'
 
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import EmojiEmotionsIcon from '@material-ui/icons/EmojiEmotions';
+import CardGiftcardIcon from '@material-ui/icons/CardGiftcard';
+import GifIcon from '@material-ui/icons/Gif';
+
+import { MuiThemeProvider } from '@material-ui/core/styles'
+import { theme as ThemeColor } from 'assets/js/style';
+import Tooltip from '@material-ui/core/Tooltip';
+
 import ChatHeader from 'components/ChatHeader'
+import Message from 'components/Message'
 export default function Chat() {
+    const iconInput = [
+        {
+            id: "126",
+            name: "Upgrade your friends! Gift them awesome chat perks with Nitro.",
+            icon: <CardGiftcardIcon/>
+        },
+        {
+            id: "126",
+            name: "Gif",
+            icon: <GifIcon/>
+        },
+        {
+            id: "126",
+            name: "Emoticon",
+            icon: <EmojiEmotionsIcon/>
+        }
+    ]
     return (
         <>
             <div className="chat__container">
                 <ChatHeader />
+
+                <div className="chat__messages">
+                    <Message />
+                </div>
+                <div className="chat__input">
+                    <AddCircleIcon fontSize="large" />
+                    <form>
+                        <input type="text" placeholder={`Message #TestChannel`}/>
+                        <button className="chat__inputButton" type="submit">Send Message</button>
+                    </form>
+
+                    <div className="chat__inputIcons">
+                        <MuiThemeProvider theme={ThemeColor}>
+                            {
+                                iconInput.map((item, index) => {
+                                    return <Tooltip key={`${index}-${item.id}`} title={item.name} placement="top">
+                                        {item.icon}
+                                    </Tooltip>
+                                })
+                            }
+                        </MuiThemeProvider>
+                    </div>
+                </div>
             </div>   
         </>
     )
--- src/components/ChatHeader.js
+++ src/components/ChatHeader.js
@@ -1,17 +1,77 @@
 import React from 'react'
 
+import { MuiThemeProvider } from '@material-ui/core/styles'
+import { theme as ThemeColor } from 'assets/js/style';
+import Tooltip from '@material-ui/core/Tooltip';
+
 import NotificationsIcon from '@material-ui/icons/Notifications';
+import EditLocationIcon from '@material-ui/icons/EditLocation';
+import SearchRounded from '@material-ui/icons/SearchRounded';
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
+import AllInboxIcon from '@material-ui/icons/AllInbox';
+import HelpIcon from '@material-ui/icons/Help';
+
 export default function ChatHeader() {
+    const listIcon = [
+        {
+            id: "123",
+            name: "Muting a channel prevents unread indicators and notifications from appearing unless you are mentioned.",
+            icon: <NotificationsIcon/>
+        },
+        {
+            id: "124",
+            name: "Pinned Messages",
+            icon: <EditLocationIcon/>
+        },
+        {
+            id: "125",
+            name: "Member List",
+            icon: <PeopleAltIcon/>
+        }
+    ]
+    const listIconTwo = [
+        {
+            id: "126",
+            name: "Inbox",
+            icon: <AllInboxIcon/>
+        },
+        {
+            id: "127",
+            name: "Help",
+            icon: <HelpIcon/>
+        }
+    ]
     return (
         <div className="chatHeader">
-            <h3>Im Header</h3>
             <div className="chatHeader__left">
                 <h3>
                     <span className="chatHeader__hash"># </span> Channel Name
                 </h3>
             </div>
             <div className="chatHeader__right">
+                <MuiThemeProvider theme={ThemeColor}>
+                    {
+                        listIcon.map((item, index) => {
+                            return <Tooltip key={`${index}-${item.id}`} title={item.name} placement="bottom">
+                                {item.icon}
+                             </Tooltip>
+                        })
+                    }
+                </MuiThemeProvider>
 
+                <div className="chatHeader__search">
+                    <input placeholder="Search"/>
+                    <SearchRounded />
+                </div>
+                <MuiThemeProvider theme={ThemeColor}>
+                    {
+                        listIconTwo.map((item, index) => {
+                            return <Tooltip key={`${index}-${item.id}`} title={item.name} placement="bottom">
+                                {item.icon}
+                             </Tooltip>
+                        })
+                    }
+                </MuiThemeProvider>
             </div>
         </div>
     )
